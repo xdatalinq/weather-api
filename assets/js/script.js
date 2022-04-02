534fdfcbd130ef9ad65d1738e3c03bb9af03c9a8
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-04-02 01:53:19 -0400

    fetch and display UV index correctly to api-container

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,11 @@
 var searchFormEl = document.querySelector("#search-form");
 var apiContainerEl = document.querySelector("#main-weather");
 var cityInputEl = document.querySelector('#city');
-var apiKey = "7c31afcd6af2016f309312b62ff32ba8";
-var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=toronto&appid=7c31afcd6af2016f309312b62ff32ba8';
 
 
 // Submit form handler
 var formSubmitHandler = function(event) {
   event.preventDefault();
-  console.log("form submit caught!");
   var city = cityInputEl.value.trim();
   if (city) {
     console.log(city);
@@ -21,49 +18,78 @@ var formSubmitHandler = function(event) {
   }
 };
 
-// Fetch
+// Fetch weather
 var getWeather = function(city) {
   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=7c31afcd6af2016f309312b62ff32ba8';
   console.log(apiUrl);
   fetch(apiUrl)
     .then(function(response) {
       if (response.ok) {
-        console.log(response);
         response.json().then(function(data) {
-          console.log(data);
           displayWeather(data);
+          
+          console.log(data);
         });
       } else {
         alert("Error: " + response.statusText);
       }
     })
     .catch(function(error) {
-      alert("Unable to connect to GitHub");
+      alert("Unable to connect to OpenWeather");
     });
 };
 
+// Fetch UV index
+var fetchUVindex = function(lat, lon) {
+  console.log('Fetch UV index start' + lat + lon);
+  var apiURL = 'https://api.openweathermap.org/data/2.5/uvi?appid=7c31afcd6af2016f309312b62ff32ba8&lat='+ lat +'&lon='+ lon;
+  fetch(apiURL)
+    .then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          console.log(data);
+          var uvIndex = data.value;
+          displayUVindex(uvIndex);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect to OpenWeather UV index");
+    });   
+};
+
+// Dislay weather, api-container
 var displayWeather = function(data) {
-  console.log("displayWeather called");
-  
-    var cityNameEl = document.createElement("h3");
-    cityNameEl.innerText = data.name;
-    apiContainerEl.appendChild(cityNameEl);
-    var currentDateEl = document.createElement("h3");
-    currentDateEl.innerText = moment().format("dddd, MMMM Do");
-    apiContainerEl.appendChild(currentDateEl);
-    var tempEl = document.createElement("h5");
-    tempEl.innerText = data.main.temp;
-    apiContainerEl.appendChild(tempEl);
-    var windEl = document.createElement("h5");
-    windEl.innerText = data.wind.speed;
-    apiContainerEl.appendChild(windEl);
-    var humidityEl = document.createElement("h5");
-    humidityEl.innerText = data.main.humidity;
-    apiContainerEl.appendChild(humidityEl);
-    // var uvIndexEl = document.createElement("h5");
-    // uvIndexEl.innerText = data[i].UVINDEX;
-    // apiContainer.appendChild(uvIndexEl);
-    
+  var lat = data.coord.lat;
+  var lon = data.coord.lon;
+  console.log(lat);
+  console.log(lon);
+  var cityNameEl = document.createElement("h3");
+  cityNameEl.innerText = data.name;
+  apiContainerEl.appendChild(cityNameEl);
+  var currentDateEl = document.createElement("h3");
+  currentDateEl.innerText = moment().format("dddd, MMMM Do");
+  apiContainerEl.appendChild(currentDateEl);
+  var tempEl = document.createElement("h5");
+  tempEl.innerText = data.main.temp;
+  apiContainerEl.appendChild(tempEl);
+  var windEl = document.createElement("h5");
+  windEl.innerText = data.wind.speed;
+  apiContainerEl.appendChild(windEl);
+  var humidityEl = document.createElement("h5");
+  humidityEl.innerText = data.main.humidity;
+  apiContainerEl.appendChild(humidityEl);
+  fetchUVindex(lat, lon);
+};
+
+// Display UV index, api-container
+var displayUVindex = function(uvi) {
+  console.log('DisplayUVindex:' + uvi);
+  var uvIndexEl = document.createElement("h5");
+  uvIndexEl.innerText = uvi;
+  apiContainerEl.appendChild(uvIndexEl);
 };
 
 // Event listeners
